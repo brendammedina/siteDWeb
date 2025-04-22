5aeb9daf58c10be726146afc1ab1014106007a7c
brendammedina <71186164+brendammedina@users.noreply.github.com>
2025-04-22 14:35:58 -0400

    Estou inserindo codigos da pagina inicial

---
+++ exercicio1.py
@@ -0,0 +1,16 @@
+#criando a lista 
+numeros = [10, 20, 30, 40, 50]
+print("Terceiro elemento:", numeros[2])
+
+#adicionando um numero à lista
+numeros.append(60)
+print("Lista adiconei 60:", numeros)
+
+#removendo um elemento
+numeros.remove(30)
+print("Lista após remover 30:", numeros)
+
+#tamanho da lista
+print("Tamanho da lista:", len(numeros))
+
+
